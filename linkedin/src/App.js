20b586d97fb6cd1223911cb449b008e57fc01c89
Daniel <dan.earp@icloud.com>
2022-12-12 11:27:17 +0000

    Profile page created

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -3,12 +3,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Components/Home'
+import Profile from './Components/Profile'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   )
