d136649204c057a70b0c3cfa8e6beeda26d13e1b
Nic <nic.dan.costea@gmail.com>
2022-12-12 16:33:35 +0100

    top-nav-styling updates

--- linkedin/src/Components/TopNav.jsx
+++ linkedin/src/Components/TopNav.jsx
@@ -1,85 +1,68 @@
-import {
-  Navbar,
-  Nav,
-  NavDropdown,
-  Form,
-  FormControl,
-  Row,
-} from "react-bootstrap";
+import { Navbar, Nav, Form, FormControl } from "react-bootstrap";
 import { SiLinkedin } from "react-icons/si";
 import { GoSearch } from "react-icons/go";
-import { HiHome, HiUserCircle } from "react-icons/hi";
+import { HiHome } from "react-icons/hi";
 import { MdPeopleAlt } from "react-icons/md";
-import { BsBriefcaseFill, BsFillGrid3X3GapFill } from "react-icons/bs";
+import { BsBriefcaseFill } from "react-icons/bs";
 import { AiFillMessage } from "react-icons/ai";
 import { FaBell } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import TopNavProfile from "./TopNavProfile";
+import TopNavWork from "./TopNavWork";
 
 const TopNav = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <div className="d-flex">
+    <Navbar expand="lg p-0 justify-content-center mt-2">
+      <div className="d-flex justify-content-center align-items-center">
         <Link href="#home">
           <SiLinkedin size={42} />
         </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <div>
+        <div className="d-flex justify-content-center align-items-center">
           <GoSearch />
           <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           </Form>
         </div>
       </div>
-      <div>
-        <Navbar.Collapse id="basic-navbar-nav">
+      <div className="d-flex justify-content-center align-items-center" id="topNavRight">
+        <Navbar.Collapse id="basic-navbar-nav" className="ml-5">
           <Nav className="mr-auto">
-            <div>
-              <Link href="#home">
-                <HiHome />
+            <div className="d-flex justify-content-center align-items-center">
+              <Link href="#home" className="top-nav-links">
+                <div className="ml-4 navbar-links">
+                  <HiHome className=" navbar-icons" />
+                  <p className="mb-0 top-nav-text-icons">Home</p>
+                </div>
               </Link>
-              <Link href="#link">
-                <MdPeopleAlt />
+              <Link href="#link" className="top-nav-links">
+                <div className="ml-4 navbar-links">
+                  <MdPeopleAlt className=" navbar-icons" />
+                  <p className="mb-0 top-nav-text-icons">My Network</p>
+                </div>
               </Link>
-              <Link>
-                <BsBriefcaseFill />
+              <Link className="top-nav-links">
+                <div className="ml-4 navbar-links">
+                  <BsBriefcaseFill className=" navbar-icons" />
+                  <p className="mb-0 top-nav-text-icons">Jobs</p>
+                </div>
               </Link>
-              <Link>
-                <AiFillMessage />
+              <Link className="top-nav-links">
+                <div className="ml-4 navbar-links">
+                  <AiFillMessage className=" navbar-icons" />
+                  <p className="mb-0 top-nav-text-icons">Messaging</p>
+                </div>
               </Link>
-              <Link>
-                <FaBell />
+              <Link className="top-nav-links">
+                <div className="ml-4 navbar-links">
+                  <FaBell className=" navbar-icons" />
+                  <p className="mb-0 top-nav-text-icons">Notifications</p>
+                </div>
               </Link>
-              <NavDropdown title={<HiUserCircle />} id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <TopNavProfile />
             </div>
             <div>
-              <NavDropdown
-                title={<BsFillGrid3X3GapFill />}
-                id="basic-nav-dropdown"
-              >
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <TopNavWork />
             </div>
           </Nav>
         </Navbar.Collapse>
