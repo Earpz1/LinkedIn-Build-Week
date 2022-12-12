51854658104e6003e5d2a0ef8fdd001a4f5dc6ab
Courtney Hampton <jaye0888@gmail.com>
2022-12-12 13:24:23 +0100

    NavBar Created

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Components/Home'
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import TopNav from "./Components/TopNav";
 
 function App() {
   return (
     <BrowserRouter>
+      <TopNav />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
