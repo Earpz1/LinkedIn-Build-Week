4e56b64156b4b3de2ee6aed4c8a0ee8e0b6ece7a
Courtney Hampton <jaye0888@gmail.com>
2022-12-13 15:55:47 +0100

    css updates
    drop downs have been altered. there is now a hidden navbar... but it does not work... so it's positioned behind the navbar.

--- linkedin/src/Components/TopNav.jsx
+++ linkedin/src/Components/TopNav.jsx
@@ -1,180 +1,161 @@
-import { Navbar, Nav, Form, FormControl, Container } from 'react-bootstrap'
-import { SiLinkedin } from 'react-icons/si'
-import { GoSearch } from 'react-icons/go'
-import { HiHome } from 'react-icons/hi'
-import { MdPeopleAlt } from 'react-icons/md'
-import { BsBriefcaseFill } from 'react-icons/bs'
-import { AiFillMessage } from 'react-icons/ai'
-import { FaBell } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-import TopNavProfile from './TopNavProfile'
-import TopNavWork from './TopNavWork'
-import { useState } from 'react'
-import { useSelector } from 'react-redux'
-import { FiDelete } from 'react-icons/fi'
-import FormSearchUser from './FormSearchUser'
+import { Navbar, Nav, Form, FormControl, Container } from "react-bootstrap";
+import { SiLinkedin } from "react-icons/si";
+import { GoSearch } from "react-icons/go";
+import { HiHome } from "react-icons/hi";
+import { MdPeopleAlt } from "react-icons/md";
+import { BsBriefcaseFill } from "react-icons/bs";
+import { AiFillMessage } from "react-icons/ai";
+import { FaBell } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import TopNavProfile from "./TopNavProfile";
+import TopNavWork from "./TopNavWork";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { FiDelete } from "react-icons/fi";
+import FormSearchUser from "./FormSearchUser";
+import ClickAwayListener from "@mui/base/ClickAwayListener";
+import HiddenNav from "./HiddenNav";
 
 const TopNav = () => {
-  const [query, setQuery] = useState('')
-  const [isQuerySet, setIsQuerySet] = useState(false)
-  const [clearInput, setClearInput] = useState(query)
-  console.log('the query is: ', query)
-  console.log('the input is: ', clearInput)
+  const [query, setQuery] = useState("");
+  const [searchSelected, setSearchSelected] = useState(false);
+  const [isQuerySet, setIsQuerySet] = useState(false);
+  const [clearInput, setClearInput] = useState(query);
+  console.log("the query is: ", query);
+  console.log("the input is: ", clearInput);
   const handleQuery = (e) => {
-    setQuery(e.target.value)
-    setIsQuerySet(true)
-  }
+    setQuery(e.target.value);
+    setIsQuerySet(true);
+  };
   const handleClearInput = () => {
     // setClearInput("");
-    setQuery('')
-    setIsQuerySet(false)
-  }
+    setQuery("");
+    setIsQuerySet(false);
+  };
   const handleSubmit = (e) => {
-    e.preventDefault()
-  }
-  const usersList = useSelector((state) => state.user.users)
-  const users = usersList[0]
-  const areUsersLoaded = useSelector((state) => state.user.usersLoaded)
+    e.preventDefault();
+  };
+  const usersList = useSelector((state) => state.user.users);
+  const users = usersList[0];
+  const areUsersLoaded = useSelector((state) => state.user.usersLoaded);
   return (
-    <Navbar expand="lg justify-content-center">
-      <Container>
-        <div
-          className="d-flex justify-content-center align-items-center"
-          id="parentDivForm"
-        >
-          <Link to="/">
-            <SiLinkedin size={38} />
-          </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <>
+      <Navbar expand="lg justify-content-center">
+        <Container>
           <div
             className="d-flex justify-content-center align-items-center"
-            id="generalSearchArea"
+            id="parentDivForm"
           >
-            <div id="topNavDeleteInput">
-              <FiDelete onClick={handleClearInput} />
-            </div>
-            <div className="pl-3 pr-2">
-              <GoSearch />
-            </div>
-            <Form inline className="searchInput" onSubmit={handleSubmit}>
-              <FormControl
-                value={query}
-                onChange={handleQuery}
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
-              />
-            </Form>
-            {isQuerySet && (
-              <ul id="topNavFormList">
-                {areUsersLoaded &&
-                  users
-                    .filter((user) => user.name.startsWith(`${query}`))
-                    .map((user) => (
-                      <li
-                        className="formListItems line-clamp-one"
-                        key={user._id}
-                      >
-                        <FormSearchUser user={user} />
-                      </li>
-                    ))}
-              </ul>
-            )}
-          </div>
-
-          {/* <div
-            className="d-flex justify-content-center align-items-center"
-            id="generalSearchArea"
-          >
-            <div className="pl-3 pr-2">
-              <GoSearch />
-            </div>
-            <Form inline className="searchInput">
-              <FormControl
-                type="text"
-                placeholder="Search"
-                className="mr-sm-2"
-              />
-            </Form>
-          </div> */}
-          {/* <div id="topNavFormDiv">
-            <FiDelete id="topNavDeleteInput" onClick={handleClearInput} />
-            <GoSearch id="topNavSerachIcon" className="ml-2" />
-            <Form onSubmit={handleSubmit}>
-              <FormControl
-                value={query}
-                onChange={handleQuery}
-                type="text"
-                placeholder="Search contacts"
-                className="mr-sm-2"
-                id="topNavFormControl"
-              />
-            </Form>
-            {isQuerySet && (
-              <ul id="topNavFormList">
-                {areUsersLoaded &&
-                  users
-                    .filter((user) => user.name.toLowerCase().startsWith(`${query}`))
-                    .slice(0, 18)
-                    .map((user) => (
-                      <li className="formListItems line-clamp-one" key={user._id}>
-                        <FormSearchUser user={user} />
-                      </li>
-                    ))}
-              </ul>
-            )}
-          </div> */}
-        </div>
-        <div
-          className="d-flex justify-content-center align-items-center"
-          id="topNavRight"
-        >
-          <Navbar.Collapse id="basic-navbar-nav" className="ml-5">
-            <Nav className="mr-auto">
-              <div className="d-flex justify-content-center align-items-center">
-                <Link to="/" className="top-nav-links">
-                  <div className="ml-4 navbar-links">
-                    <HiHome className=" navbar-icons" />
-                    <p className="mb-0 top-nav-text-icons">Home</p>
-                  </div>
-                </Link>
-                <Link href="#link" className="top-nav-links">
-                  <div className="ml-4 navbar-links">
-                    <MdPeopleAlt className=" navbar-icons" />
-                    <p className="mb-0 top-nav-text-icons">My Network</p>
-                  </div>
-                </Link>
-                <Link className="top-nav-links">
-                  <div className="ml-4 navbar-links">
-                    <BsBriefcaseFill className=" navbar-icons" />
-                    <p className="mb-0 top-nav-text-icons">Jobs</p>
-                  </div>
-                </Link>
-                <Link className="top-nav-links">
-                  <div className="ml-4 navbar-links">
-                    <AiFillMessage className=" navbar-icons" />
-                    <p className="mb-0 top-nav-text-icons">Messaging</p>
-                  </div>
-                </Link>
-                <Link className="top-nav-links">
-                  <div className="ml-4 navbar-links">
-                    <FaBell className=" navbar-icons" />
-                    <p className="mb-0 top-nav-text-icons">Notifications</p>
-                  </div>
-                </Link>
-                <div className="topNavMeBox">
-                  <TopNavProfile />
+            <Link to="/">
+              <SiLinkedin size={38} />
+            </Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <ClickAwayListener onClickAway={() => setSearchSelected(false)}>
+              <div
+                id="generalSearchArea"
+                className={
+                  searchSelected === true
+                    ? "d-flex justify-content-center align-items-center searchSetSelected"
+                    : "d-flex justify-content-center align-items-center"
+                }
+              >
+                <div id="topNavDeleteInput">
+                  <FiDelete onClick={handleClearInput} />
                 </div>
+                <div className="pl-3 pr-2">
+                  <GoSearch />
+                </div>
+                <Form inline className="searchInput" onSubmit={handleSubmit}>
+                  <input
+                    value={query}
+                    onChange={handleQuery}
+                    type="text"
+                    placeholder="Search"
+                    className="mr-sm-2"
+                    onClick={() => setSearchSelected(true)}
+                  />
+                </Form>
+                {isQuerySet && (
+                  <ul id="topNavFormList">
+                    {areUsersLoaded &&
+                      users
+                        .filter((user) =>
+                          user.name.toLowerCase().startsWith(`${query}`)
+                        )
+                        .slice(0, 10)
+                        .map((user) => (
+                          <li
+                            className="formListItems line-clamp-one pl-2"
+                            key={user._id}
+                          >
+                            <FormSearchUser user={user} />
+                          </li>
+                        ))}
+                    {areUsersLoaded && (
+                      <p
+                        className="text-center font-weight-bold"
+                        style={{ borderTop: "solid 1pt #c6c5c3" }}
+                      >
+                        <Link>See all results</Link>
+                      </p>
+                    )}
+                  </ul>
+                )}
               </div>
-              <div className="topNavWorkBox">
-                <TopNavWork />
-              </div>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
-      </Container>
-    </Navbar>
-  )
-}
+            </ClickAwayListener>
+          </div>
+          <div
+            className="d-flex justify-content-center align-items-center"
+            id="topNavRight"
+          >
+            <Navbar.Collapse id="basic-navbar-nav" className="ml-5">
+              <Nav className="mr-auto">
+                <div className="d-flex justify-content-center align-items-center">
+                  <Link to="/" className="top-nav-links">
+                    <div className="ml-4 navbar-links">
+                      <HiHome className=" navbar-icons" />
+                      <p className="mb-0 top-nav-text-icons">Home</p>
+                    </div>
+                  </Link>
+                  <Link href="#link" className="top-nav-links">
+                    <div className="ml-4 navbar-links">
+                      <MdPeopleAlt className=" navbar-icons" />
+                      <p className="mb-0 top-nav-text-icons">My Network</p>
+                    </div>
+                  </Link>
+                  <Link className="top-nav-links">
+                    <div className="ml-4 navbar-links">
+                      <BsBriefcaseFill className=" navbar-icons" />
+                      <p className="mb-0 top-nav-text-icons">Jobs</p>
+                    </div>
+                  </Link>
+                  <Link className="top-nav-links">
+                    <div className="ml-4 navbar-links">
+                      <AiFillMessage className=" navbar-icons" />
+                      <p className="mb-0 top-nav-text-icons">Messaging</p>
+                    </div>
+                  </Link>
+                  <Link className="top-nav-links">
+                    <div className="ml-4 navbar-links">
+                      <FaBell className=" navbar-icons" />
+                      <p className="mb-0 top-nav-text-icons">Notifications</p>
+                    </div>
+                  </Link>
+                  <div className="topNavMeBox">
+                    <TopNavProfile />
+                  </div>
+                </div>
+                <div className="topNavWorkBox">
+                  <TopNavWork />
+                </div>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+        </Container>
+      </Navbar>
+      <HiddenNav />
+    </>
+  );
+};
 
-export default TopNav
+export default TopNav;
--- linkedin/src/Components/TopNavProfile.jsx
+++ linkedin/src/Components/TopNavProfile.jsx
@@ -1,4 +1,4 @@
-import { NavDropdown } from "react-bootstrap";
+import { Button, NavDropdown } from "react-bootstrap";
 import { HiHome, HiUserCircle } from "react-icons/hi";
 
 const TopNavProfile = () => {
@@ -8,10 +8,23 @@ const TopNavProfile = () => {
         <HiUserCircle size={20} />
       </div>
       <NavDropdown title="Me" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item>
+          <div className="d-flex flex-column firstnavitem">
+            <div className="d-flex mb-2">
+              <div>
+                <HiUserCircle size={48} />
+              </div>
+              <div>
+                <p className="m-0">username</p>
+                <p className="m-0">user position</p>
+              </div>
+            </div>
+            <Button className="rounded-pill">View Profile</Button>
+          </div>
+          <NavDropdown.Divider />
+        </NavDropdown.Item>
         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
       </NavDropdown>
     </>
