6155722316ac8ca091c12b8a2cad9d2a271b3626
Daniel <dan.earp@icloud.com>
2022-12-14 14:21:45 +0000

    Main layout done

---
+++ linkedin/src/Components/HomePageLeft.jsx
@@ -0,0 +1,11 @@
+const HomePageLeft = () => {
+  return (
+    <>
+      <div className="left-feed-box">
+        <h1>Left side content</h1>
+      </div>
+    </>
+  )
+}
+
+export default HomePageLeft
---
+++ linkedin/src/Components/NewsFeedItem.jsx
@@ -0,0 +1,11 @@
+const NewsFeedItem = () => {
+  return (
+    <>
+      <div className="news-feed-post mt-4">
+        <p>A news feed post will go here</p>
+      </div>
+    </>
+  )
+}
+
+export default NewsFeedItem
