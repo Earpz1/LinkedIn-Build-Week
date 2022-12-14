95855fc70ead856ae263d718342616a9a2306e69
Nic <nic.dan.costea@gmail.com>
2022-12-14 17:54:22 +0100

    fetching the posts list

--- linkedin/src/Components/Home.jsx
+++ linkedin/src/Components/Home.jsx
@@ -1,31 +1,46 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react'
-import {
-  fetchUsers,
-  fetchProfile,
-  currentUser,
-  fetchExperiences,
-} from '../redux/actions'
-import { Container, Row, Col } from 'react-bootstrap'
-import HomePageLeft from './HomePageLeft'
-import CreateNewPost from './CreateNewPost'
-import HomePageRight from './HomePageRight'
-import NewsFeedItem from './NewsFeedItem'
-import { AiFillCaretDown } from 'react-icons/ai'
-import ProfileRight from './ProfileRight'
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchUsers, fetchProfile, currentUser, fetchExperiences, fetchPostsList } from "../redux/actions";
+import { Container, Row, Col } from "react-bootstrap";
+import HomePageLeft from "./HomePageLeft";
+import CreateNewPost from "./CreateNewPost";
+import HomePageRight from "./HomePageRight";
+import NewsFeedItem from "./NewsFeedItem";
+import { AiFillCaretDown } from "react-icons/ai";
+import ProfileRight from "./ProfileRight";
 
 const Home = () => {
-  const dispatch = useDispatch()
-  const usersData = useSelector((state) => state.user.users)
-  const currentUserData = useSelector((state) => state.user.currentUser)
-  const usersLoaded = useSelector((state) => state.user.usersLoaded)
-  const contact = useSelector((state) => state.user.contact)
+  const dispatch = useDispatch();
+  const usersData = useSelector((state) => state.user.users);
+  const currentUserData = useSelector((state) => state.user.currentUser);
+  const usersLoaded = useSelector((state) => state.user.usersLoaded);
+  const contact = useSelector((state) => state.user.contact);
+  const postsList = useSelector((state) => state.posts.posts.postsList);
+  console.log("the posts are: ", postsList);
+
+  // const currentUserPosts = () => {
+  //   // let result = [];
+  //   if (postsList.length && postsList.length > 0) {
+  //     // result = postsList[0].filter((post) => post.user._id === "5fc4c2beed266800170ea3d4");
+  //     console.log(
+  //       "Stefano's posts are:",
+  //       postsList[0].filter((post) => post.user._id === "5fc4c2beed266800170ea3d4")
+  //     );
+  //   } else {
+  //     console.log("something is misssssing....");
+  //   }
+  //   //console.log("the result is: ", result);
+  // };
+  // currentUserPosts();
+
+  //console.log("the posts for the current user are: ", currentUserPosts(postsList));
 
   useEffect(() => {
-    dispatch(fetchProfile())
-    dispatch(fetchExperiences(currentUserData._id))
-    dispatch(fetchUsers())
-  }, [])
+    dispatch(fetchProfile());
+    dispatch(fetchExperiences(currentUserData._id));
+    dispatch(fetchUsers());
+    dispatch(fetchPostsList());
+  }, []);
 
   return (
     <>
@@ -40,15 +55,15 @@ const Home = () => {
               <div className="d-flex align-items-center">
                 <hr className="mt-3" />
                 <span>
-                  Sort by: Top{' '}
+                  Sort by: Top{" "}
                   <a href="#">
                     <AiFillCaretDown />
                   </a>
                 </span>
               </div>
-              <NewsFeedItem />
-              <NewsFeedItem />
-              <NewsFeedItem />
+              {postsList &&
+                postsList[0].length > 0 &&
+                postsList[0].slice(0, 5).map((post) => <NewsFeedItem key={post._id} post={post} />)}
             </Row>
           </Col>
           <Col lg={3} className="px-0">
@@ -57,7 +72,7 @@ const Home = () => {
         </Row>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
